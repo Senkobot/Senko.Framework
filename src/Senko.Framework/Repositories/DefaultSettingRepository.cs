47723505dec62e478ad7e1fc7d354160c1bd7e55
Gerard Smit <gsmit1996@gmail.com>
2019-08-14 23:49:59 +0200

    Added setting repository (#6)

---
+++ src/Senko.Framework/Repositories/DefaultSettingRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senko.Framework.Repositories
+{
+    internal class DefaultSettingRepository : ISettingRepository
+    {
+        public Task<string> GetAsync(ulong guildId, string key)
+        {
+            return Task.FromResult(string.Empty);
+        }
+
+        public Task SetAsync(ulong guildId, string key, string value)
+        {
+            throw new NotSupportedException("The setting repository is not provided. Please register the ISettingRepository.");
+        }
+    }
+}
