c520fd5c1b529a6dfe762c7b9983aa89e526e29c
Gerard Smit <gsmit1996@gmail.com>
2019-09-10 00:23:09 +0200

    Auto-register the Redis client, message bus and hybrid cache

--- src/Senko.Framework/Hosting/BotApplication.cs
+++ src/Senko.Framework/Hosting/BotApplication.cs
@@ -23,7 +23,13 @@ namespace Senko.Framework.Hosting
         private readonly ILogger<BotApplication> _logger;
         private readonly IServiceProvider _provider;
 
-        public BotApplication(IServiceProvider provider, IApplicationBuilderFactory builderFactory, IMessageContextFactory messageContextFactory, IMessageContextAccessor contextAccessor, IDiscordClient client, ILogger<BotApplication> logger)
+        public BotApplication(
+            IServiceProvider provider,
+            IApplicationBuilderFactory builderFactory,
+            IMessageContextFactory messageContextFactory,
+            IMessageContextAccessor contextAccessor,
+            IDiscordClient client,
+            ILogger<BotApplication> logger)
         {
             _provider = provider;
             _messageContextFactory = messageContextFactory;
